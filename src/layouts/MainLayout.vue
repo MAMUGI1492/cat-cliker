cf8f853fac65c23df40f5075b74aa1552f2c3481
MAMUGI1492 <mmg1492@gmail.com>
2021-07-01 01:29:46 +0200

    🎨 Refactor: Adjust the name of the identifiers

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title>Cat-Cliker</q-toolbar-title>
+        <q-toolbar-title>StyleSage technical test</q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
