a20421ecf1ca3b1b0cafb1afe7911d7d3bc45a29
MAMUGI1492 <mmg1492@gmail.com>
2021-07-07 04:12:54 +0200

    🚜 Refactor: Scaffolding improvement IV

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,69 +1,14 @@
 <template>
-  <q-page class="page-index">
-    <q-stepper v-model="step" color="primary" ref="stepper" animated>
-      <q-step :done="hasEndedFirstStep" :name="1" icon="person" title="Paso 1">
-        To do component
-      </q-step>
-
-      <q-step :done="hasEndedSecondStep" :name="2" icon="group" title="Paso 2">
-        To do component
-      </q-step>
-
-      <q-step :name="3" icon="space_dashboard" title="Paso 3">
-        To do component
-      </q-step>
-
-      <template v-slot:navigation>
-        <q-stepper-navigation>
-          <q-btn
-            @click="forward"
-            :disable="hasEnded"
-            color="primary"
-            label="Continuar"
-          />
-
-          <q-btn
-            v-if="hasBegin"
-            @click="backward"
-            color="primary"
-            label="Atrás"
-            flat
-          />
-        </q-stepper-navigation>
-      </template>
-    </q-stepper>
-  </q-page>
+  <q-page class="page-index"> </q-page>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
-import { QStepper } from 'quasar';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'PageIndex',
   setup() {
-    const stepper = ref(QStepper);
-    const step = ref(1);
-
-    const hasEndedFirstStep = computed(() => step.value > 1);
-    const hasEndedSecondStep = computed(() => step.value > 2);
-    const hasEnded = computed(() => step.value >= 3);
-    const hasBegin = computed(() => step.value > 1);
-
-    const forward = () => stepper.value.next();
-
-    const backward = () => stepper.value.previous();
-
-    return {
-      stepper,
-      step,
-      hasBegin,
-      hasEndedFirstStep,
-      hasEndedSecondStep,
-      hasEnded,
-      forward,
-      backward,
-    };
+    return {};
   },
 });
 </script>
@@ -73,10 +18,5 @@ export default defineComponent({
   display: flex;
   justify-content: center;
   align-items: center;
-
-  .q-stepper .q-stepper__nav {
-    display: flex;
-    gap: 8px;
-  }
 }
 </style>
